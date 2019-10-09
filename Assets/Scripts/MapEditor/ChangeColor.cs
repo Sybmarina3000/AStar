924bebf3b9603e8b4ec6a596917d2f56b1bed547
sybmarina3000 <sybmarina3000@gmail.com>
2019-10-09 23:48:44 +0300

    finish version; (Add paint color buttons)

---
+++ Assets/Scripts/MapEditor/ChangeColor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColor : MonoBehaviour
+{
+   private Image _image;
+   [SerializeField] private Color[] mass;
+
+   private void Start()
+   {
+      _image = GetComponent<Image>();
+   }
+
+   public void ChangeColorImage(int numberColor)
+   {
+      _image.color = mass[numberColor];
+   }
+
+}
